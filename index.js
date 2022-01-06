989028bfa149c7c707d2ad133cc4267a9684f493
mmyona <hyon2001@ajou.ac.kr>
2022-01-06 23:51:10 +0900

    01.06

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const $=document;
-const root=$.querySelector('#root')
+// express server
+const express = require("express");
+const path = require("path");
+
+// express 사용
+const app = express();
+
+app.use(express.static(__dirname+"/public"));
+
+app.get("/*", (req, res) => {
+    res.sendFile(__dirname+"index.html");
+});
+
+// port 생성 서버 실행
+app.listen(process.env.PORT || 3000, () => console.log("Server running ....")) 
