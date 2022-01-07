f5f64b1a0c01f1f9eee724a5c2f601967f13d895
mmyona <hyon2001@ajou.ac.kr>
2022-01-07 23:58:41 +0900

    SPA 라우팅 시스템 구현 따라해봄

---
+++ index.js
@@ -0,0 +1,23 @@
+// css
+require('./css/style.css')
+
+// router
+const {
+  initialRoutes,
+  historyRouterPush,
+} = require('./router')
+
+const historyAppDiv = document.querySelector('#history-app')
+
+// Browser History
+initialRoutes('history', historyAppDiv)
+
+window.onload = () => {
+  const historyLinker = document.querySelectorAll('span.history')
+  historyLinker.forEach(el => {
+    el.addEventListener('click', (evt) => {
+      const pathName = evt.target.getAttribute('route')
+      historyRouterPush(pathName, historyAppDiv)
+    })
+  })
+}
